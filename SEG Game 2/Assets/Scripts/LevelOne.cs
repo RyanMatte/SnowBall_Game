11eb25f2011f39547ee352b383a50c5adf7bd9ab
Samuel Daly <sdaly093@uottawa.ca>
2020-04-01 19:12:41 -0400

    Final Game v1.0

---
+++ SEG Game 2/Assets/Scripts/LevelOne.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOne : MonoBehaviour
+{
+
+    public GameObject howToPlayUI;
+    public GameObject scoreUI;
+
+    void Start()
+    {
+        howToPlayUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Mouse0)){
+        if (howToPlayUI.activeSelf == true){
+          howToPlayUI.SetActive(false);
+          scoreUI.SetActive(true);
+          Time.timeScale = 1f;
+          PauseMenu.GameIsPaused = false;
+        }
+      }
+    }
+}
--- SEG Game 2/Assets/Scripts/PauseMenu.cs
+++ SEG Game 2/Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,6 +11,21 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    public GameObject howToPlayUI;
+    public GameObject scoreUI;
+
+    public GameObject sphere;
+
+    [SerializeField]
+    public Text ScoreText;
+
+    public int score;
+
+
+
+
+
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -22,16 +38,42 @@ public class PauseMenu : MonoBehaviour
                 Pause();
             }
         }
+        Vector3 p = sphere.transform.position;
+        Vector3 s = sphere.transform.localScale;
+
+        if (p[1] == 184.104){
+          ScoreText.text = "0";
+        }
+        else {
+          score = (int)(p[1] - 184.104) * -1;
+          ScoreText.text = score.ToString();
+        }
+
+
+
+        Debug.Log((p[1] - s[1]) - p[1] - s[1]);
+        Debug.Log(s[1]);
+        Debug.Log(p[1]);
+
+
+
     }
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
+        scoreUI.SetActive(true);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
+
+    public void LevelOne(){
+      SceneManager.LoadScene("Scenes/Level 1");
+    }
+
     void Pause()
     {
         pauseMenuUI.SetActive(true);
+        scoreUI.SetActive(false);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
